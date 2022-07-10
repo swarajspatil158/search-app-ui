7a1a9763041be0aced4bb06d6e88020f16740cea
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-10 19:10:35 +0530

    search bar styling done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import styles from "./searchBarStyle.module.scss";
+import searchIcon from "../assets/searchIcon.png";
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      <div className={`${styles.search}`}>
+        <input
+          type="text"
+          className={`${styles.searchTerm}`}
+          placeholder="Search"
+        />
+        <button type="submit" className={`${styles.searchButton}`}>
+          <img
+            src={searchIcon}
+            className={`${styles.searchButtonImg}`}
+            alt="search-icon"
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import SearchBar from "../components/SearchBar";
 import LatestTrends from "../components/LatestTrends";
 import styles from "./home.module.scss";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return (
     <div className={`${styles.mainBg}`}>
-      <SearchBar />
-      <LatestTrends />
+      <img className={`${styles.logo}`} src={logo} alt="logo" />
+
+      <div className={`${styles.flexPosition}`}>
+        <SearchBar />
+        <LatestTrends />
+      </div>
     </div>
   );
 };
