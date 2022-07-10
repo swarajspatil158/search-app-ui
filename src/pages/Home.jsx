8571d73e9d57648fb12078ecb93905194f926961
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-10 13:47:28 +0530

    sass setup and home background added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import LatestTrends from "../components/LatestTrends";
+import styles from "./home.module.scss";
 
 const Home = () => {
   return (
-    <div>
+    <div className={`${styles.mainBg}`}>
       <SearchBar />
       <LatestTrends />
     </div>
